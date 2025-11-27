ae1fe0109e3a49cfc528b474e4e9fe5604a437b9
Renato Díaz <re.diazp@duocuc.cl>
2025-11-27 17:41:02 -0300

    microservicio de productos

---
+++ ms_productos/src/main/java/com/example/ms_productos/MsProductosApplication.java
@@ -0,0 +1,13 @@
+package com.example.ms_productos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsProductosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsProductosApplication.class, args);
+	}
+
+}
---
+++ ms_productos/src/test/java/com/example/ms_productos/MsProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ms_productos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsProductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
